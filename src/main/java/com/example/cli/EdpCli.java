f71cadde4812d10745b1c32912d0733aae64577c
aarsh-nagarro <aarsh.mishra@nagarro.com>
2025-08-20 22:48:29 +0530

    Update EdpCli.java

--- src/main/java/com/example/cli/EdpCli.java
+++ src/main/java/com/example/cli/EdpCli.java
@@ -1,5 +1,7 @@
 package com.example.cli;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -13,6 +15,8 @@ import picocli.CommandLine.Option;
 )
 public class EdpCli implements Runnable {
 
+    private static final Logger LOGGER = Logger.getLogger(EdpCli.class.getName());
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new EdpCli()).execute(args);
         System.exit(exitCode);
@@ -20,30 +24,34 @@ public class EdpCli implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Use a subcommand like say-hello or say-bye.");
+        LOGGER.log(Level.INFO, "Use a subcommand like say-hello or say-bye.");
     }
 }
 
 @Command(name = "say-hello", description = "Say hello to someone")
 class SayHello implements Runnable {
 
+    private static final Logger LOGGER = Logger.getLogger(SayHello.class.getName());
+
     @Option(names = {"--name"}, description = "Name to greet", required = true)
     String name;
 
     @Override
     public void run() {
-        System.out.println("Hello, " + name + "!");
+        LOGGER.log(Level.INFO, "Hello, {0}!", name);
     }
 }
 
 @Command(name = "say-bye", description = "Say goodbye to someone")
 class SayBye implements Runnable {
 
+    private static final Logger LOGGER = Logger.getLogger(SayBye.class.getName());
+
     @Option(names = {"--name"}, description = "Name to say bye to", required = true)
     String name;
 
     @Override
     public void run() {
-        System.out.println("Bye, " + name + "!");
+        LOGGER.log(Level.INFO, "Bye, {0}!", name);
     }
 }
